4344fbe7bb989c5d0f0e98c8149a0bbd95d180cb
Gabriel Zamai <gabriel.zamai@bkbank.com.br>
2025-01-07 09:05:09 -0300

    Implemented LinearGradient, Image and Statusbar

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -3,6 +3,9 @@ import { theme } from '@/theme';
 import { useUserStore } from '@/store/userStore';
 import { router } from 'expo-router';
 import { PlantlyButton } from '@/components/PlantlyButton';
+import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar } from 'expo-status-bar';
+import { PlantlyImage } from '@/components/PlantlyImage';
 
 export default function OnboardingScreen() {
   const toggleHasOnboarded = useUserStore((state) => state.toggleHasOnboarded);
@@ -13,21 +16,43 @@ export default function OnboardingScreen() {
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Onboarding</Text>
-      <PlantlyButton title="Onboard" onPress={handlePressButton} />
-    </View>
+    <LinearGradient
+      start={{ x: 0, y: 0 }}
+      colors={[theme.colorGreen, theme.colorAppleGreen, theme.colorLimeGreen]}
+      end={{ x: 1, y: 1 }}
+      style={styles.container}
+    >
+      <View>
+        <Text style={styles.heading}>Plantly</Text>
+        <Text style={styles.tagline}>Keep Your plants Happy and Hydrated</Text>
+      </View>
+      <PlantlyImage />
+      <PlantlyButton title="Let me in!" onPress={handlePressButton} />
+      <StatusBar style="light" />
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
     backgroundColor: theme.colorWhite,
   },
   text: {
     fontSize: 24,
+  },
+  heading: {
+    fontSize: 42,
+    color: theme.colorWhite,
+    fontWeight: 'bold',
+    marginBottom: 12,
+    textAlign: 'center',
+  },
+  tagline: {
+    fontSize: 24,
+    color: theme.colorWhite,
+    textAlign: 'center',
   },
 });
--- theme.ts
+++ theme.ts
@@ -1,5 +1,7 @@
 export const theme = {
+  colorAppleGreen: '#a0d36c',
   colorGreen: '#29b365',
   colorLeafyGreen: '#206a42',
+  colorLimeGreen: '#d0e57e',
   colorWhite: '#fff',
 };
